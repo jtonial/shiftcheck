92ff14669f8dbaed3193a50ee9792131d29aa6dd
Nick <nrmitchi@gmail.com>
2012-09-21 22:34:54 -0400

    Fixing typo

--- public_html/js/admin-application.js
+++ public_html/js/admin-application.js
@@ -33,12 +33,12 @@ $(function() {
 	});
 
 	window.Schedule = Backbone.Model.extend({
-		url: '/api/schedules?date='+this.get('date')+'&sessionOverride=1';
+		url: '/api/schedules?date='+this.get('date')+'&sessionOverride=1',
 		model: Shift,
 
 		initialize: function () {
 			this.shifts = new Shifts();
-			this.shifts.url='/api/schedules?date='+this.get('date')+'&sessionOverride=1';
+			this.shifts.url='/api/schedules?date='+this.get('date')+'&sessionOverride=1',
 		}
 
 	});
