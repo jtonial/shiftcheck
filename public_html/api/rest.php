aae4e46abde5619ddb382c0f92fc105864831a2a
Nick <nrmitchi@gmail.com>
2012-09-23 11:48:45 -0400

    Bug fixed

--- public_html/api/rest.php
+++ public_html/api/rest.php
@@ -406,7 +406,7 @@ class RestSQL {
 	function getSchedule ($date) {
 		$this->trace[]='getSchedule';
 		try {
-			$query = "SELECT s.shift_id,s.employer_id,s.date,s.start_time,s.end_time,s.position,s.employee_id,e.name, e.enumber, e.phone FROM shifts as s JOIN employees as e USING (employee_id) WHERE date=DATE('$date') and employer_id='$this->id' ORDER BY date,start_time ASC";
+			$query = "SELECT s.shift_id,s.employer_id,s.date,s.start_time,s.end_time,s.position,s.employee_id,e.name, e.enumber, e.phone FROM shifts as s JOIN employees as e USING (employee_id) WHERE date=DATE('$date') and s.employer_id='$this->id' ORDER BY date,start_time ASC";
 			$this->return['query']=$query;
 			$resource=$this->db->query($query);
 			if (!$resource) {
