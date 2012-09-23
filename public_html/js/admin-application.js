aa61be01f4d182ff8f50abd2606a9fe26d68c753
Nick <nrmitchi@gmail.com>
2012-09-23 18:56:25 -0400

    Formatting date on schedule tabs

--- public_html/js/admin-application.js
+++ public_html/js/admin-application.js
@@ -129,11 +129,12 @@ $(function() {
 			var Days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 			var Months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
-			var d=schedule.get('date');//'2012-01-01';//This will be the real date
+			var datenum=schedule.get('date');//'2012-01-01';//This will be the real date
+			var d = new Date(datenum);
 			var datestring = Days[d.getDay()]+', '+Months[d.getMonth()]+' '+d.getDate()+1; //This will be the date string
-			this.$('#dates.nav-tabs div').before('<li><a href="#'+d+'" data-toggle="tab">'+datestring+'</a></li>');
+			this.$('#dates.nav-tabs div').before('<li><a href="#'+datenum+'" data-toggle="tab">'+datestring+'</a></li>');
 
-			this.$('.tab-content').append('<div class="tab-pane" id="'+d+'"></div>');
+			this.$('.tab-content').append('<div class="tab-pane" id="'+datenum+'"></div>');
 			
 		},
 	});
