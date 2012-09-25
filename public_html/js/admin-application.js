b0492ea43696cc46134a1b0c42ee7d94c652072b
Nick <nrmitchi@gmail.com>
2012-09-25 19:55:03 -0400

    Adding superscripts to date

--- public_html/js/admin-application.js
+++ public_html/js/admin-application.js
@@ -152,8 +152,8 @@ $(function() {
 			var datenum=schedule.get('date');//'2012-01-01';//This will be the real date
 			var d = new Date(datenum);
 			var datestring = Days[d.getDay()]+', '+Months[d.getMonth()]+' '+(d.getDate()+1); //This will be the date string
-			console.log(sups[(d.getDate+1)%10]);
-			this.$('#dates.nav-tabs div').before('<li><a href="#'+datenum+'" data-toggle="tab">'+datestring+'<sup>'+sups[(d.getDate+1)%10]+'</sup></a></li>');
+			console.log(Sups[(d.getDate+1)%10]);
+			this.$('#dates.nav-tabs div').before('<li><a href="#'+datenum+'" data-toggle="tab">'+datestring+'<sup>'+Sups[(d.getDate+1)%10]+'</sup></a></li>');
 
 			var view = new Scheduleme.classes.views.ScheduleView ({model:schedule});
 
