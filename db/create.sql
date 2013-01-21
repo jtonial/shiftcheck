a4cd41a1316a3eb7fa6f45f900bfbdb9bb8c8fa1
Nicholas Mitchinson <nrmitchi@Nicholass-MacBook-Air.local>
2013-01-20 22:14:26 -0800

    Adding SQL create and test data

--- db/create.sql
+++ db/create.sql
@@ -1,44 +1,83 @@
-CREATE TABLE employees {
-	employee_id INT NOT NULL (PRIMARY KEY),
-	email varchar(40) NOT NULL,
-	password char(40) NOT NULL,
-	first_name varchar(20) NOT NULL,
-	last_name varchar(20) NOT NULL,
-	employer INT NOT NULL,
-	last_login DATE NOT NULL,
-	login_count INT NOT NULL,
-	reg_time DATE NOT NULL,
+CREATE DATABASE IF NOT EXISTS scheduleme;
 
-	FOREIGN KEY (employer) REFERENCES employers
-}
-CREATE TABLE employers {
-	employer_id INT NOT NULL (PRIMARY KEY),
+use scheduleme;
+
+DROP TABLE IF EXISTS shifts;
+DROP TABLE IF EXISTS schedules;
+DROP TABLE IF EXISTS employee_positions;
+DROP TABLE IF EXISTS positions;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS employers;
+
+CREATE TABLE employers (
+	employer_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name varchar(15) NOT NULL,
 	email varchar(40) NOT NULL,
+	username varchar(15) NOT NULL,
 	password char(40) NOT NULL,
 	contact_email varchar(20) NOT NULL,
 	contact_phone varchar(20) NOT NULL,
 	contact_address varchar(20) NOT NULL,
+
 	img varchar(50) NOT NULL,
+
+	last_login DATE NOT NULL,
+	login_count INT NOT NULL DEFAULT 0,
+	reg_time DATE NOT NULL,
+
+	CONSTRAINT UNIQUE (email)
+
+) ENGINE=innodb;
+
+CREATE TABLE employees (
+	employee_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email varchar(40) NOT NULL,
+	username varchar(15) NOT NULL,
+	password char(40) NOT NULL,
+	first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	employer_id INT UNSIGNED NOT NULL,
 	last_login DATE NOT NULL,
 	login_count INT NOT NULL,
 	reg_time DATE NOT NULL,
-}
-CREATE TABLE positions {
-	employer INT NOT NULL,
+
+	CONSTRAINT FOREIGN KEY (employer_id) REFERENCES employers(employer_id)
+
+) ENGINE=innodb;
+
+CREATE TABLE positions (
+	position_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	employer_id INT UNSIGNED NOT NULL,
 	position varchar(5) NOT NULL,
 
-	PRIMARY KEY (employer, position),
-	FOREIGN KEY (employer) REFERENCES employers
-}
-CREATE TABLE employee_positions {
+	CONSTRAINT FOREIGN KEY (employer_id) REFERENCES employers(employer_id)
+
+) ENGINE=innodb;
+
+CREATE TABLE employee_positions (
 	/*How do I reference the compound primary key in positions?*/
 	employee INT NOT NULL,
-	position ____ NOT NULL
-}
-CREATE TABLE schedules {
-	schedule_id INT NOT NULL (PRIMARY KEY)
-	employer INT NOT NULL
-}
-CREATE TABLE shifts {
-	
-}
+	position INT UNSIGNED NOT NULL
+
+) ENGINE=innodb;
+
+CREATE TABLE schedules (
+	schedule_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	employer_id INT UNSIGNED NOT NULL,
+
+	date DATE NOT NULL,
+	type ENUM('day','week','twoweek','month') NOT NULL,
+	creation_time DATETIME NOT NULL,
+	image_loc varchar(45) NOT NULL,
+
+	CONSTRAINT FOREIGN KEY (employer_id) REFERENCES employers(employer_id)
+
+) ENGINE=innodb;
+
+CREATE TABLE shifts (
+	shift_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	schedule_id INT UNSIGNED NOT NULL,
+
+	CONSTRAINT FOREIGN KEY (schedule_id) REFERENCES schedules(schedule_id)
+
+) ENGINE=innodb;
---
+++ db/testData.sql
@@ -0,0 +1,18 @@
+use scheduleme;
+
+INSERT INTO employers (`employer_id`, `name`, `email`, `username`, `password`, `contact_email`, `contact_phone`, `contact_address`, `last_login`, `reg_time`)
+	VALUES 	(1, 'McDonalds', 'mcds@nritchi.ca', 'mcds', 'ce3e810d5a1c96a77e55d5a2d79fc73dc5a5e6db', 'mcds@nrmitchi.ca', '123-456-7890', '6410 Millcreek Drive', NOW(), NOW()),
+			(2, 'Tim Hortons', 'timmies@nrmitchi.ca', 'timmies', '1840fe111f0021c49af9463f45236216d2ab1cea', 'timmies@nrmitchi.ca', '012-345-6789', '6666 Winston Churchill Blvd', NOW(), NOW());
+
+INSERT INTO employees (`employee_id`, `email`, `username`, `password`, `first_name`, `last_name`, `employer_id`, `last_login`, `reg_time`)
+	VALUES	(1, 'nick@nrmitchi.ca', 'nick', '01b3ad3301a0b79e9bae3f9b1feb6431a5513687', 'Nick', 'Mitchinson', 1, NOW(), NOW()),
+			(2, 'jack@nrmitchi.ca', 'jack', '9a71241f7c521dd59795b8886f43c2011b567b3e', 'Jack', 'Johnson', 1, NOW(), NOW()),
+			(3, 'john@nrmitchi.ca', 'john', '2464438fce2dd0a04d56b748c70f87f260517d3d', 'John', 'Jackson', 1, NOW(), NOW()),
+			(4, 'steve@nrmitchi.ca', 'steve', '', 'Steve', 'Stephenson', 2, NOW(), NOW()),
+			(5, 'holly@nrmitchi.ca', 'holly', '', 'Holly', 'Smithson', 2, NOW(), NOW());
+
+INSERT INTO schedules (`schedule_id`, `employer_id`, `date`, `type`, `creation_time`, `image_loc`)
+	VALUES	(1, 1, '2013-01-20', 'day', NOW(), '12BarzTickets.pdf'),
+			(2, 1, '2013-01-21', 'day', NOW(), '12BarzTickets.pdf'),
+			(3, 1, '2013-01-22', 'day', NOW(), '12BarzTickets.pdf'),
+			(4, 1, '2013-01-23', 'week', NOW(), '12BarzTickets.pdf');
