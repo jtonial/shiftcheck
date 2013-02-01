7791bc2fcb92c166219275c7532d04d373db6943
Nick Mitchinson <nrmitchi@gmail.com>
2013-01-31 23:37:46 -0800

    Updating environment config to HOST

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -72,7 +72,7 @@ var database = require('mysql-simple');
 // Port number is optional
 
 if (typeof process.env.CLEARDB_DATABASE_USER != 'undefined'){
-	database.init(process.env.CLEARDB_DATABASE_USER, process.env.CLEARDB_DATABASE_PASSWORD, process.env.CLEARDB_DATABASE_DB, process.env.CLEARDB_DATABASE_URL, 3306);
+	database.init(process.env.CLEARDB_DATABASE_USER, process.env.CLEARDB_DATABASE_PASSWORD, process.env.CLEARDB_DATABASE_DB, process.env.CLEARDB_DATABASE_HOST, 3306);
 } else {
 	database.init('root', 'password', 'scheduleme', 'localhost', 3306);
 }
