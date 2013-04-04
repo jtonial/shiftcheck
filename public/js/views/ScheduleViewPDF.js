11a0d22bcd26dd3b3597d65395ef8b6a5dca37d6
Nick Mitchinson <nrmitchi@gmail.com>
2013-04-04 00:39:40 -0700

    a

--- public/js/views/ScheduleViewPDF.js
+++ public/js/views/ScheduleViewPDF.js
@@ -1,5 +1,5 @@
 Scheduleme.classes.views.ScheduleView.pdf = Scheduleme.classes.views.ScheduleBaseView.extend({
 
-	template: Handlebars.compile($('#schedule-template').html()),
+	template: Handlebars.compile($('#schedule-template').html())
 	
 });
