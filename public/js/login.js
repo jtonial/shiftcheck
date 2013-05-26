bb43dd113d4e23c0cdcbb33a3f8b7eed33210498
Nick Mitchinson <nrmitchi@gmail.com>
2013-05-26 15:09:41 -0400

    Fixing a bug that strict introduced... sorta

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,22 @@
 (function () {
   
   "use strict"
+  $(document).ready( function () {
+    $('#login-form').submit(function (e) {
+      console.log('test');
+      e.preventDefault(); //Prevent default submission
+      console.log($(this).serialize());
 
-  $('#login-form').submit(function (e) {
-    console.log('test');
-    e.preventDefault(); //Prevent default submission
-    console.log($(this).serialize());
-
-    $.ajax({
-      url: window.location.pathname+'?sendTime='+(new Date()).getTime(),
-      type: 'POST',
-      data: $(this).serialize(),
-      success: function (response) {
-        window.location.href='/';
-      }, error: function (response) {
-        alert ('Username and password do not match. Please try again');
-      }
-    });  
+      $.ajax({
+        url: window.location.pathname+'?sendTime='+(new Date()).getTime(),
+        type: 'POST',
+        data: $(this).serialize(),
+        success: function (response) {
+          window.location.href='/';
+        }, error: function (response) {
+          alert ('Username and password do not match. Please try again');
+        }
+      });  
+    });
   });
-
 })();
