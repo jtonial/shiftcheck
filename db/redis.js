165426bd6fa4b09b7a52ca26f3293c82fb5799c9
Nick Mitchinson <nrmitchi@gmail.com>
2013-06-27 21:40:15 -0400

    omg

--- db/redis.js
+++ db/redis.js
@@ -9,7 +9,10 @@ var Redis = require('redis');
 var redis = Redis.createClient(Main.Config.redis.port, Main.Config.redis.hostname);
 
 if (Main.Config.redis.password) {
+  console.log('Authing redis connection');
   redis.auth(Main.Config.redis.password);
 }
+
+console.log(Main.Config.redis);
 
 module.exports = redis;
